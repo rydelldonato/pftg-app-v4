ab53560b2469712019c233a238c3702f06ad03cd
Rydell <donatorydell98@gmail.com>
2023-08-14 18:52:12 -0700

    initial commit for animated button

--- ui/orderPage/menuItemModal/menuItemModal.js
+++ ui/orderPage/menuItemModal/menuItemModal.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import Modal from "react-native-modal";
 import * as Animatable from "react-native-animatable"; // Import Animatable
 import CloseButton from "../../closeButton";
+import { Button } from "react-native";
 
 export default function MenuItemModal(props) {
   const { modalVisible, setModalVisible } = props;
@@ -38,7 +39,32 @@ export default function MenuItemModal(props) {
             animation="fadeIn"
             delay={300}
           >
-            <Text>menuItemModal</Text>
+            <Text>FoodItem</Text>
+            <Text>price</Text>
+            <View style={{ width: "100%", height: "100%" }}>
+              <Image
+                style={{ width: "100%", height: "100%" }}
+                source={require("../../../assets/lumpia.png")}
+              />
+            </View>
+            <Text>Ingredients</Text>
+            <Animatable.View
+              style={{width: 247 }}
+              animation="rubberBand"
+              iterationCount="infinite" // Make the animation infinite
+              iterationDelay={1000} // Delay between each iteration
+            >
+              <TouchableOpacity
+                style={styles.customButton} // Apply custom button style
+                onPress={() => {
+                  // Handle button press here
+                }}
+              >
+                <Text style={styles.buttonTitle}>
+                  Order Now Through Doordash
+                </Text>
+              </TouchableOpacity>
+            </Animatable.View>
           </Animatable.View>
         </Animatable.View>
       </Modal>
@@ -67,5 +93,16 @@ const styles = StyleSheet.create({
     zIndex: 1,
     top: 310,
     right: 244,
+  },
+  customButton: {
+    backgroundColor: "#007BFF", // Customize button background color
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+  },
+  buttonTitle: {
+    fontSize: 15, // Adjust the font size as needed
+    color: "white", // Customize text color
+    textAlign: "center",
   },
 });
